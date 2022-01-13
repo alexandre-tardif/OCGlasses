e6c324ee204d08fb4bb2d1eae94ff7e0fbb27615
Florexiz <florexiz.nm@gmail.com>
2022-01-13 15:25:26 +0300

    Set item now require database address and index in it You can now create item stack in DB with code anyway

--- src/main/java/com/bymarcin/openglasses/surface/widgets/core/attribute/IItemable.java
+++ src/main/java/com/bymarcin/openglasses/surface/widgets/core/attribute/IItemable.java
@@ -3,6 +3,6 @@ package com.bymarcin.openglasses.surface.widgets.core.attribute;
 import net.minecraft.item.ItemStack;
 
 public interface IItemable extends IAttribute {
-    boolean setItem(String name, int meta);
+    boolean setItem(ItemStack itemStack);
     ItemStack getItem();
 }
--- src/main/java/com/bymarcin/openglasses/surface/widgets/core/luafunction/SetItem.java
+++ src/main/java/com/bymarcin/openglasses/surface/widgets/core/luafunction/SetItem.java
@@ -1,11 +1,14 @@
 package com.bymarcin.openglasses.surface.widgets.core.luafunction;
 
 import com.bymarcin.openglasses.surface.widgets.core.attribute.IItemable;
+import li.cil.oc.api.internal.Database;
 import li.cil.oc.api.machine.Arguments;
 import li.cil.oc.api.machine.Context;
 
 import com.bymarcin.openglasses.lua.LuaFunction;
 import com.bymarcin.openglasses.surface.Widget;
+import li.cil.oc.api.network.Environment;
+import net.minecraft.item.ItemStack;
 
 public class SetItem extends LuaFunction{
 
@@ -13,9 +16,15 @@ public class SetItem extends LuaFunction{
     public Object[] call(Context context, Arguments arguments) {
         Widget widget = getSelf().getWidget();
         if(widget instanceof IItemable){
-            ((IItemable) widget).setItem(arguments.checkString(0), arguments.checkInteger(1));
-            getSelf().getTerminal().updateWidget(getSelf().getWidgetRef());
-            return null;
+            Environment env = context.node().network().node(arguments.checkString(0)).host();
+            if (env instanceof Database) {
+                ItemStack itemStack = ((Database) env).getStackInSlot(arguments.checkInteger(1) - 1);
+                ((IItemable) widget).setItem(itemStack);
+                getSelf().getTerminal().updateWidget(getSelf().getWidgetRef());
+                return null;
+            }
+            throw new RuntimeException("Not a database");
+
         }
         throw new RuntimeException("Component does not exists!");
     }
